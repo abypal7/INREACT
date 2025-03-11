4f1c7970fe328bac6d3dea7c5d2a760bac3dedaa
Abhinav pal <aby@Abhinavs-MacBook-Pro.local>
2025-03-11 13:32:35 +0530

    commit first

---
+++ app.js
@@ -0,0 +1,16 @@
+const heading= React.createElement("h1",{},"Hello React 2nd day");
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+//now to do nested div and further operations
+const Parent= React.createElement("div",{id:"Parent"},
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1",{},"Hello i am nested h1"),
+        React.createElement("h2",{},"Nested H2 Tag"),]),
+        React.createElement("div",{id:"child2"},[
+            React.createElement("h3",{},"A nESTED Take on heading 3"),
+            React.createElement("h4",{},"A nESTED Take on heading 4"),
+        ]),
+
+);
+        console.log(Parent);
+        root.render(Parent);
